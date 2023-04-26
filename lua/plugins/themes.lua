81aa92e4f8be70fa427cbf084eb71478f48f9ef5
Yorick Chan <yorick@gmail.com>
2023-04-27 01:24:54 +0800

    add golang, update syntax color to seti

---
+++ lua/plugins/themes.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "seti",
+    },
+  },
+  { "tanvirtin/monokai.nvim" },
+  { "trusktr/seti.vim" },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+}
